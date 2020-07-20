7bc08b90338f53a8b40a16eb7a23195a78d0f670
achiastuti <sriastuti.achi29@gmail.com>
2020-07-20 13:05:36 +0700

    ujian modul 1

--- soal-1.py
+++ soal-1.py
@@ -1,11 +1,13 @@
-
 def hashtag(word):
-    word.lower()
-    if (len(word)) > 0:
-      word = word.replace(' ', '')
-      print(word)
-    else :
-      print('false')
-hashtag('Hello There How Are You Doing')
-hashtag ('   hello world   ')
-hashtag('')
+  hashtag = ""
+  if (len(word)) > 0:
+    spl = word.split(' ')
+    for i in range (0, len(spl)):
+      hashtag += spl[i].capitalize()
+    return '#'+hashtag
+  else :
+    return False
+
+print(hashtag('Hello There How Are You Doing'))
+print(hashtag ('   hello world   '))
+print(hashtag(''))
